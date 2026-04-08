1d79d43ba8d5d99614cd790e76a2af82e31362b8
ilaha <ilaha.mammedzade@gmail.com>
2026-04-08 19:24:23 +0400

    Added favicon to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Head } from 'next/head';
+
 import { PageShell } from '@/components/layout/page-shell';
 import { HomeExperience } from '@/components/sections/home/home-experience';
 
@@ -7,6 +9,9 @@ export default function HomePage() {
   return (
     <PageShell>
       <HomeExperience />
+      <Head>
+        <link rel="icon" href='/Favicon.png' />
+      </Head>
     </PageShell>
   );
 }
